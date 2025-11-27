d4ee5066f6f1230a9372d05c1bc786a84217aa8d
FranPedernera <pancho25pedernera@gmail.com>
2025-11-27 15:42:52 -0300

    Se hizo login y register

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+//import './Auth.css';
+//import AuthForm from './components/AuthForm';
+//import supabase from './supabaseClient';
+import Login from'./components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Login />
   );
 }
 
